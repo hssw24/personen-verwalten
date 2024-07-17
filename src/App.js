08a06eb6981ca28139fdba0932749a5afae5aae3
hssw24 <hssw@gmx.de>
2024-07-17 19:26:40 +0200

    Erste Version zus mit ChatGpt

--- src/App.js
+++ src/App.js
@@ -1,4 +1,80 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+  const [persons, setPersons] = useState([]);
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+  const [editIndex, setEditIndex] = useState(null);
+
+  useEffect(() => {
+    fetchPersons();
+  }, []);
+
+  const fetchPersons = async () => {
+    const response = await axios.get('http://localhost:5000/persons');
+    setPersons(response.data);
+  };
+
+  const addPerson = async () => {
+    if (editIndex !== null) {
+      await axios.put(`http://localhost:5000/persons/${editIndex}`, { name, number });
+      setEditIndex(null);
+    } else {
+      await axios.post('http://localhost:5000/persons', { name, number });
+    }
+    setName('');
+    setNumber('');
+    fetchPersons();
+  };
+
+  const deletePerson = async (index) => {
+    await axios.delete(`http://localhost:5000/persons/${index}`);
+    fetchPersons();
+  };
+
+  const editPerson = (index) => {
+    setName(persons[index].name);
+    setNumber(persons[index].number);
+    setEditIndex(index);
+  };
+
+  return (
+    <div className="App">
+      <h1>Personen Verwalten</h1>
+      <input 
+        type="text" 
+        placeholder="Name" 
+        value={name} 
+        onChange={(e) => setName(e.target.value)} 
+      />
+      <input 
+        type="number" 
+        placeholder="Zahl" 
+        value={number} 
+        onChange={(e) => setNumber(e.target.value)} 
+      />
+      <button onClick={addPerson}>
+        {editIndex !== null ? 'Aktualisieren' : 'Hinzufügen'}
+      </button>
+      <ul>
+        {persons.map((person, index) => (
+          <li key={index}>
+            {person.name} - {person.number}
+            <button onClick={() => editPerson(index)}>Bearbeiten</button>
+            <button onClick={() => deletePerson(index)}>Löschen</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
+
+
+/*import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -23,3 +99,4 @@ function App() {
 }
 
 export default App;
+*/
